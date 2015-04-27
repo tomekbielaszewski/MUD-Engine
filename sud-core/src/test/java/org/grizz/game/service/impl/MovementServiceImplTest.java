238113aef42bea7f8df815bf78755647c6af1fe3
Grizwold <tomekbielaszewski@gmail.com>
2015-04-27 07:51:33 +0200

    Rozpoczęto pisanie testów...

---
+++ sud-core/src/test/java/org/grizz/game/service/impl/MovementServiceImplTest.java
@@ -0,0 +1,24 @@
+package org.grizz.game.service.impl;
+
+import org.grizz.game.config.GameConfig;
+import org.grizz.game.service.MovementService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Grizz on 2015-04-26.
+ */
+@ContextConfiguration(classes = {GameConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MovementServiceImplTest {
+    @Autowired
+    private MovementService movementService;
+
+    @Test
+    public void test() {
+
+    }
+}
