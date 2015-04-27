a5262396b0d53a1461a44c3443aa742f0507909b
Grizwold <tomekbielaszewski@gmail.com>
2015-04-27 21:59:42 +0200

    Sprzatanie

--- sud-core/src/main/java/org/grizz/game/GameImpl.java
+++ sud-core/src/main/java/org/grizz/game/GameImpl.java
@@ -15,10 +15,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GameImpl implements Game {
-    //    @Autowired
-//    private ScriptEngine scriptEngine;
-//    @Autowired
-//    private MovementService movementService;
     @Autowired
     private CommandHandlerBus commandHandlerBus;
     @Autowired
@@ -30,48 +26,6 @@ public class GameImpl implements Game {
     public PlayerResponse runCommand(String command, String player) {
         PlayerContext context = playerRepository.get(player);
         commandHandlerBus.execute(command, context);
-
-//        ScriptContext context = scriptEngine.getContext();
-//        context.setWriter(new OutputStreamWriter(System.out));
-//        try {
-//            scriptEngine.eval("print('Dziala!');", context);
-//        } catch (ScriptException e) {
-//            e.printStackTrace();
-//        }
-//
-//        PlayerContextImpl grizz = new PlayerContextImpl("Grizz",
-//                100,
-//                50,
-//                20,
-//                25,
-//                45,
-//                80,
-//                "1",
-//                "1",
-//                Maps.newHashMap(),
-//                Lists.newArrayList());
-//        PlayerContextImpl grizzPlusStr = grizz.copy().strength(101).build();
-//
-//        System.out.println("Grizz:  " + grizz);
-//        System.out.println("Grizz+: " + grizzPlusStr);
-//
-//        System.out.println("Moving Grizz NORTH");
-//        movementService.move(Direction.NORTH, grizzPlusStr);
-//
-//        System.out.println(grizzPlusStr);
-//
-//        System.out.println("Moving Grizz NORTH");
-//        movementService.move(Direction.NORTH, grizzPlusStr);
-//        System.out.println(grizzPlusStr);
-//
-//        commandHandlerBus.execute("south", grizzPlusStr);
-//        System.out.println(grizzPlusStr);
-//
-//        commandHandlerBus.execute("east", grizzPlusStr);
-//        System.out.println(grizzPlusStr);
-//
-//        commandHandlerBus.execute("east", grizzPlusStr);
-//        System.out.println(grizzPlusStr);
 
         PlayerResponse response = responseExtractor.extract(context);
         resetContext(context);
