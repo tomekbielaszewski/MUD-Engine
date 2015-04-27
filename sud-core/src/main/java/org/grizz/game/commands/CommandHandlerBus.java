954df2c2aa9f2000b0a92c2c7f934935b07a6458
tomekbielaszewski <tomekbielaszewski@gmail.com>
2015-04-27 17:13:34 +0200

    Dodano zabezpieczenie przeciw duplikacji CommandHandlerów w CommandHandlerBusie

--- sud-core/src/main/java/org/grizz/game/commands/CommandHandlerBus.java
+++ sud-core/src/main/java/org/grizz/game/commands/CommandHandlerBus.java
@@ -1,15 +1,15 @@
 package org.grizz.game.commands;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.grizz.game.model.PlayerContext;
 
-import java.util.List;
+import java.util.Set;
 
 /**
  * Created by tomasz.bielaszewski on 2015-04-27.
  */
 public class CommandHandlerBus {
-    private List<Command> commands = Lists.newArrayList();
+    private Set<Command> commands = Sets.newHashSet();
 
     public void execute(String strCommand, PlayerContext context) {
         for (Command command : commands) {
@@ -21,6 +21,8 @@ public class CommandHandlerBus {
     }
 
     public void addCommand(Command command) {
-        commands.add(command);
+        if (!commands.add(command)) {
+            throw new IllegalArgumentException("Duplicated Command! Only one instance of " + command.getClass().getCanonicalName() + " allowed");
+        }
     }
 }
