1fe51014dcbc9009253863de38369e99f67aa355
tomekbielaszewski <tomekbielaszewski@gmail.com>
2015-08-20 17:04:36 +0200

    rozpoczeta implementacja skryptu kowadła

--- sud-core/src/main/resources/scripts/js/items/blackSmithAnvilCraftingScript.js
+++ sud-core/src/main/resources/scripts/js/items/blackSmithAnvilCraftingScript.js
@@ -1,3 +1,17 @@
+var ironOre = "401";
+var coal = "402";
+var roughStone = "403";
+var silverNugget = "404";
+var goldNugget = "405";
+var mithril = "406";
+
+var recipes = [
+    {id: "407", ingredients: [ironOre, ironOre, coal]}, //sztabka zelaza
+    {id: "408", ingredients: [goldNugget, goldNugget, goldNugget, goldNugget]}, //sztabka zlota
+    {id: "409", ingredients: [silverNugget, silverNugget, silverNugget, silverNugget]}, //sztabka srebra
+    {id: "410", ingredients: [mithril, mithril, mithril, mithril]}  //sztabka mithrilu
+];
+
 function result() {
     response.getPlayerEvents().add("Kujesz kujesz i wykuc nie mozesz... :P");
     return true;
