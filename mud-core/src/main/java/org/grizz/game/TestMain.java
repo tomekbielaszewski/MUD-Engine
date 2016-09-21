c0f5f1cc2f2a5a19fe1284291bf0e4060862ae3d
tomekbielaszewski <tomekbielaszewski@gmail.com>
2016-09-21 17:36:58 +0200

    player not exist exception text

--- mud-core/src/main/java/org/grizz/game/TestMain.java
+++ mud-core/src/main/java/org/grizz/game/TestMain.java
@@ -3,6 +3,6 @@ package org.grizz.game;
 public class TestMain {
     public static void main(String[] args) {
         Game game = GameFactory.getInstance((playerName, response) -> System.out.println(playerName + ": " + response.toString()));
-        game.runCommand("grizz", "lista graczy");
+        game.runCommand("lista graczy", "grizz");
     }
 }
