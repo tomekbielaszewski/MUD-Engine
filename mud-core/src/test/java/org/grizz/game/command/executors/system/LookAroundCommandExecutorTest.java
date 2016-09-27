5bcca91cfc6d6bcebb7739f454763e6d4b430d8c
Grizwold <tomekbielaszewski@gmail.com>
2016-09-27 22:59:40 +0200

    look around command executor tests

--- mud-core/src/test/java/org/grizz/game/command/executors/system/LookAroundCommandExecutorTest.java
+++ mud-core/src/test/java/org/grizz/game/command/executors/system/LookAroundCommandExecutorTest.java
@@ -4,14 +4,16 @@ import com.google.common.collect.Lists;
 import org.grizz.game.model.Location;
 import org.grizz.game.model.Player;
 import org.grizz.game.model.PlayerResponse;
+import org.grizz.game.model.Script;
 import org.grizz.game.model.repository.LocationRepo;
 import org.grizz.game.model.repository.PlayerRepository;
+import org.grizz.game.model.repository.ScriptRepo;
+import org.grizz.game.service.script.ScriptRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.not;
@@ -30,9 +32,14 @@ public class LookAroundCommandExecutorTest {
     private static final String PLAYER_NAME = "name1";
     private static final String PLAYER_NAME_2 = "name2";
     private static final String PLAYER_NAME_3 = "name3";
+    private static final String SCRIPT_ID = "script";
 
     @Mock
     private LocationRepo locationRepo;
+    @Mock
+    private ScriptRepo scriptRepo;
+    @Mock
+    private ScriptRunner scriptRunner;
     @Mock
     private PlayerRepository playerRepository;
 
@@ -54,6 +61,8 @@ public class LookAroundCommandExecutorTest {
     public void findsOtherPlayersOnLocation() throws Exception {
         PlayerResponse response = new PlayerResponse();
         Player player = dummyPlayer(PLAYER_ID, PLAYER_NAME, LOCATION_ID);
+        Location location = Location.builder().id(LOCATION_ID).build();
+        when(locationRepo.get(LOCATION_ID)).thenReturn(location);
         when(playerRepository.findByCurrentLocation(LOCATION_ID)).thenReturn(Lists.newArrayList(
                 player,
                 dummyPlayer(PLAYER_ID_2, PLAYER_NAME_2, LOCATION_ID),
@@ -71,7 +80,17 @@ public class LookAroundCommandExecutorTest {
 
     @Test
     public void callsOnShowScript() throws Exception {
-        throw new NotImplementedException();
+        PlayerResponse response = new PlayerResponse();
+        Player player = dummyPlayer(PLAYER_ID, PLAYER_NAME, LOCATION_ID);
+        Location location = Location.builder().id(LOCATION_ID).onShowScript(SCRIPT_ID).build();
+        Script script = Script.builder().id(SCRIPT_ID).build();
+        when(locationRepo.get(LOCATION_ID)).thenReturn(location);
+        when(scriptRepo.get(SCRIPT_ID)).thenReturn(script);
+
+        executor.lookAround(player, response);
+
+        verify(scriptRepo).get(SCRIPT_ID);
+        verify(scriptRunner).execute(script, player, response);
     }
 
     private Player dummyPlayer(String id, String name, String location) {
