9c4d5dc86d8c53063c0c9426d3ddad461bdcbe26
Grizwold <tomekbielaszewski@gmail.com>
2016-10-22 14:07:41 +0200

    removing items from locations  with LocationService is tested

--- mud-core/src/main/java/org/grizz/game/service/LocationService.java
+++ mud-core/src/main/java/org/grizz/game/service/LocationService.java
@@ -1,41 +1,60 @@
 package org.grizz.game.service;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
-import org.grizz.game.exception.CantDropStaticItemException;
+import old.org.grizz.game.exception.NoSuchItemException;
+import old.org.grizz.game.exception.NotEnoughItemsException;
+import org.grizz.game.exception.CantMoveStaticItemException;
 import org.grizz.game.exception.InvalidAmountException;
 import org.grizz.game.model.Location;
 import org.grizz.game.model.items.Item;
 import org.grizz.game.model.items.ItemType;
+import org.grizz.game.model.repository.ItemRepo;
 import org.grizz.game.model.repository.LocationItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
-
-import static org.springframework.util.CollectionUtils.isEmpty;
+import java.util.stream.IntStream;
 
 @Slf4j
 @Service
 public class LocationService {
     @Autowired
     private LocationItemsRepository locationItemsRepository;
+    @Autowired
+    private ItemRepo itemRepo;
 
     public List<Item> removeItems(String name, int amount, Location location) {
-        return Collections.emptyList();
+        validateAmount(amount, "cant.pick.up.none.items");
+
+        Item item = itemRepo.getByName(name);
+        validateIfStatic(Lists.newArrayList(item), "cant.pick.up.static.item");
+
+        List<Item> removedItems = removeItemsFromLocation(item, amount, location);
+        saveLocationItems(location);
+
+        return removedItems;
     }
 
     public void addItems(List<Item> items, Location location) {
-        if(isEmpty(items)) {
-            throw new InvalidAmountException("cant.drop.none.items");
-        }
-
-        if(containsStaticItems(items)) {
-            throw new CantDropStaticItemException("cant.drop.static.item");
-        }
+        validateAmount(items.size(), "cant.drop.none.items");
+        validateIfStatic(items, "cant.drop.static.item");
 
         addItemsToLocation(items, location);
         saveLocationItems(location);
+    }
+
+    private void validateAmount(int amount, String message) {
+        if (amount <= 0) {
+            throw new InvalidAmountException(message);
+        }
+    }
+
+    private void validateIfStatic(List<Item> items, String message) {
+        if (containsStaticItems(items)) {
+            throw new CantMoveStaticItemException(message);
+        }
     }
 
     private boolean containsStaticItems(List<Item> items) {
@@ -46,6 +65,22 @@ public class LocationService {
     private void addItemsToLocation(List<Item> items, Location location) {
         List<Item> mobileItems = location.getItems().getMobileItems();
         mobileItems.addAll(items);
+    }
+
+    private List<Item> removeItemsFromLocation(Item itemToRemove, int amount, Location location) {
+        List<Item> mobileItems = location.getItems().getMobileItems();
+        long amountOfMatchingItems = mobileItems.stream()
+                .filter(i -> i.getName().equals(itemToRemove.getName()))
+                .count();
+        if (amountOfMatchingItems == 0) throw new NoSuchItemException("");
+        if (amountOfMatchingItems < amount) throw new NotEnoughItemsException("");
+
+        List<Item> removedItems = Lists.newArrayList();
+        IntStream.range(0, amount).forEach(i -> {
+            mobileItems.remove(itemToRemove);
+            removedItems.add(itemToRemove);
+        });
+        return removedItems;
     }
 
     private void saveLocationItems(Location location) {
--- mud-core/src/test/java/org/grizz/game/service/LocationServiceTest.java
+++ mud-core/src/test/java/org/grizz/game/service/LocationServiceTest.java
@@ -1,28 +1,33 @@
 package org.grizz.game.service;
 
 import com.google.common.collect.Lists;
-import org.grizz.game.exception.CantDropStaticItemException;
+import old.org.grizz.game.exception.NoSuchItemException;
+import old.org.grizz.game.exception.NotEnoughItemsException;
+import org.grizz.game.exception.CantMoveStaticItemException;
 import org.grizz.game.exception.InvalidAmountException;
 import org.grizz.game.model.Location;
 import org.grizz.game.model.LocationItems;
 import org.grizz.game.model.items.Armor;
 import org.grizz.game.model.items.Item;
 import org.grizz.game.model.items.Static;
+import org.grizz.game.model.repository.ItemRepo;
 import org.grizz.game.model.repository.LocationItemsRepository;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LocationServiceTest {
@@ -32,14 +37,19 @@ public class LocationServiceTest {
     private static final String ITEM_4 = "itemName 4";
     private static final String ITEM_5 = "itemName 5";
 
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
     @Mock
     private LocationItemsRepository locationItemsRepository;
+    @Mock
+    private ItemRepo itemRepo;
 
     @InjectMocks
     private LocationService locationService = new LocationService();
 
     @Test
-    public void manyDroppedItemsAreAddedToEmptyLocation() throws Exception {
+    public void manyItemsAreAddedToEmptyLocation() throws Exception {
         Location location = dummyLocation(Lists.newArrayList());
         List<Item> items = dummyItems(ITEM_1, ITEM_2, ITEM_3);
 
@@ -56,7 +66,7 @@ public class LocationServiceTest {
     }
 
     @Test
-    public void manyDroppedItemsAreAddedToNonEmptyLocation() throws Exception {
+    public void manyItemsAreAddedToNonEmptyLocation() throws Exception {
         Location location = dummyLocation(Lists.newArrayList(dummyItems(ITEM_4, ITEM_5)));
         List<Item> items = dummyItems(ITEM_1, ITEM_2, ITEM_3);
 
@@ -75,7 +85,7 @@ public class LocationServiceTest {
     }
 
     @Test
-    public void singleDroppedItemIsAddedToEmptyLocation() throws Exception {
+    public void singleItemIsAddedToEmptyLocation() throws Exception {
         Location location = dummyLocation(Lists.newArrayList());
         List<Item> items = dummyItems(ITEM_1);
 
@@ -88,7 +98,7 @@ public class LocationServiceTest {
     }
 
     @Test
-    public void singleDroppedItemIsAddedToNonEmptyLocation() throws Exception {
+    public void singleItemIsAddedToNonEmptyLocation() throws Exception {
         Location location = dummyLocation(Lists.newArrayList(dummyItems(ITEM_4, ITEM_5)));
         List<Item> items = dummyItems(ITEM_1);
 
@@ -104,27 +114,146 @@ public class LocationServiceTest {
         verify(locationItemsRepository).save(location.getItems());
     }
 
-    @Test(expected = InvalidAmountException.class)
-    public void throwsExceptionWhenDroppingZeroItems() throws Exception {
+    @Test
+    public void throwsExceptionWhenAddingZeroItems() throws Exception {
         Location location = dummyLocation(Lists.newArrayList());
         List<Item> items = Lists.newArrayList();
-
-        locationService.addItems(items, location);
-    }
-
-    @Test(expected = CantDropStaticItemException.class)
-    public void throwsExceptionWhenDroppingStaticItems() throws Exception {
-        Location location = dummyLocation(Lists.newArrayList());
-        List<Item> items = Lists.newArrayList(
-                Static.builder().build()
-        );
+        expectedException.expect(InvalidAmountException.class);
 
         locationService.addItems(items, location);
     }
 
     @Test
-    public void pickItems() throws Exception {
-        throw new NotImplementedException();
+    public void throwsExceptionWhenAddingStaticItems() throws Exception {
+        Location location = dummyLocation(Lists.newArrayList());
+        List<Item> items = Lists.newArrayList(
+                Static.builder().build()
+        );
+        expectedException.expect(CantMoveStaticItemException.class);
+
+        locationService.addItems(items, location);
+    }
+
+    @Test
+    public void removesSingleExistingItem() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+
+        List<Item> removedItems = locationService.removeItems(ITEM_1, 1, location);
+
+        assertThat(removedItems, hasSize(1));
+        assertThat(removedItems, hasItem(dummyItem(ITEM_1)));
+        assertThat(location.getItems().getMobileItems(), hasSize(0));
+        assertThat(location.getItems().getStaticItems(), hasSize(0));
+        verify(locationItemsRepository).save(location.getItems());
+    }
+
+    @Test
+    public void removesMostOfExistingItems() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1, ITEM_1, ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+
+        List<Item> removedItems = locationService.removeItems(ITEM_1, 2, location);
+
+        assertThat(removedItems, hasSize(2));
+        assertThat(removedItems, hasItem(dummyItem(ITEM_1)));
+        assertThat(location.getItems().getMobileItems(), hasSize(1));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_1)));
+        assertThat(location.getItems().getStaticItems(), hasSize(0));
+        verify(locationItemsRepository).save(location.getItems());
+    }
+
+    @Test
+    public void removesMostOfExistingItemsWhenOtherItemsAlsoExist() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1, ITEM_1, ITEM_1, ITEM_2, ITEM_3, ITEM_2, ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+
+        List<Item> removedItems = locationService.removeItems(ITEM_1, 3, location);
+
+        assertThat(removedItems, hasSize(3));
+        assertThat(removedItems, hasItem(dummyItem(ITEM_1)));
+        assertThat(removedItems, not(hasItem(dummyItem(ITEM_2))));
+        assertThat(removedItems, not(hasItem(dummyItem(ITEM_3))));
+        assertThat(location.getItems().getMobileItems(), hasSize(4));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_1)));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_2)));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_3)));
+        assertThat(location.getItems().getStaticItems(), hasSize(0));
+        verify(locationItemsRepository).save(location.getItems());
+    }
+
+    @Test
+    public void throwsExceptionWhenRemovingTooManyItems() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+        expectedException.expect(NotEnoughItemsException.class);
+
+        locationService.removeItems(ITEM_1, 2, location);
+
+        assertThat(location.getItems().getMobileItems(), hasSize(1));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_1)));
+        verify(locationItemsRepository, never()).save(location.getItems());
+    }
+
+    @Test
+    public void throwsExceptionWhenRemovingNonExistingItem() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_2);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+        expectedException.expect(NoSuchItemException.class);
+
+        locationService.removeItems(ITEM_1, 1, location);
+
+        assertThat(location.getItems().getMobileItems(), hasSize(1));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_2)));
+        verify(locationItemsRepository, never()).save(location.getItems());
+    }
+
+    @Test
+    public void throwsExceptionWhenRemovingStaticItem() throws Exception {
+        List<Item> itemsOnLocation = dummyItems();
+        Location location = dummyLocation(itemsOnLocation);
+        location.getItems().setStaticItems(Lists.newArrayList(dummyStatic(ITEM_1)));
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyStatic(ITEM_1));
+        expectedException.expect(CantMoveStaticItemException.class);
+
+        locationService.removeItems(ITEM_1, 1, location);
+
+        assertThat(location.getItems().getMobileItems(), hasSize(0));
+        assertThat(location.getItems().getStaticItems(), hasSize(1));
+        verify(locationItemsRepository, never()).save(location.getItems());
+    }
+
+    @Test
+    public void throwsExceptionWhenRemovingZeroItems() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+        expectedException.expect(InvalidAmountException.class);
+
+        locationService.removeItems(ITEM_1, 0, location);
+
+        assertThat(location.getItems().getMobileItems(), hasSize(1));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_1)));
+        verify(locationItemsRepository, never()).save(location.getItems());
+    }
+
+    @Test
+    public void throwsExceptionWhenRemovingNegativeNumberOfItems() throws Exception {
+        List<Item> itemsOnLocation = dummyItems(ITEM_1);
+        Location location = dummyLocation(itemsOnLocation);
+        when(itemRepo.getByName(ITEM_1)).thenReturn(dummyItem(ITEM_1));
+        expectedException.expect(InvalidAmountException.class);
+
+        locationService.removeItems(ITEM_1, -1, location);
+
+        assertThat(location.getItems().getMobileItems(), hasSize(1));
+        assertThat(location.getItems().getMobileItems(), hasItem(dummyItem(ITEM_1)));
+        verify(locationItemsRepository, never()).save(location.getItems());
     }
 
     private Location dummyLocation(List<Item> mobileItems) {
@@ -136,7 +265,7 @@ public class LocationServiceTest {
                 .build();
     }
 
-    private Armor dummyItem(String name) {
+    private Item dummyItem(String name) {
         return Armor.builder().name(name).build();
     }
 
@@ -146,5 +275,9 @@ public class LocationServiceTest {
             items.add(dummyItem(names[i]));
         }
         return items;
+    }
+
+    private Static dummyStatic(String name) {
+        return Static.builder().name(name).build();
     }
 }
