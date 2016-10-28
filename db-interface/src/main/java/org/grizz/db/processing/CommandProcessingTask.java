bdf806951e9a3f5be5621df78e66f769e3d76b0f
Grizwold <tomekbielaszewski@gmail.com>
2016-10-28 19:51:59 +0200

    TODO removed

--- db-interface/src/main/java/org/grizz/db/processing/CommandProcessingTask.java
+++ db-interface/src/main/java/org/grizz/db/processing/CommandProcessingTask.java
@@ -22,7 +22,7 @@ public class CommandProcessingTask implements Runnable {
 
     @Override
     public void run() {
-        List<PlayerCommand> commands = playerCommandRepository.findByProcessedOrderByTimestampAsc(false);//TODO sort by timestamp
+        List<PlayerCommand> commands = playerCommandRepository.findByProcessedOrderByTimestampAsc(false);
 
         if(!commands.isEmpty())
             log.info("Fetched {} unprocessed commands", commands.size());
