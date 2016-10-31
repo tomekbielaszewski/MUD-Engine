73b0755dcf4f6ca7679c1ec40c0b25b3ce4a3388
tomekbielaszewski <tomekbielaszewski@gmail.com>
2016-10-31 16:08:10 +0100

    removed unused repository query method

--- db-interface/src/main/java/org/grizz/db/model/repository/ProcessedPlayerResponseRepository.java
+++ db-interface/src/main/java/org/grizz/db/model/repository/ProcessedPlayerResponseRepository.java
@@ -3,8 +3,5 @@ package org.grizz.db.model.repository;
 import org.grizz.db.model.ProcessedPlayerResponse;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface ProcessedPlayerResponseRepository extends MongoRepository<ProcessedPlayerResponse, String> {
-    List<ProcessedPlayerResponse> findBySent(boolean sent);
 }
