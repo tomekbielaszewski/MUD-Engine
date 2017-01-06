90ac85f34c71f8f81873497706cfc56062881b46
Grizwold <tomekbielaszewski@gmail.com>
2017-01-06 19:51:53 +0100

    scripts stubs for some tutorial quest

---
+++ assets/scripts/js/quests/tutorial/canPickupPackage.js
@@ -0,0 +1,10 @@
+//@ sourceURL=assets/scripts/js/quests/tutorial/canPickupPackage.js
+//line above is for IntelliJ debugging purposes
+
+(function () {
+  function canPickupPackage() {
+    return true;
+  }
+
+  return canPickupPackage();
+})();
---
+++ assets/scripts/js/quests/tutorial/respawnPackage.js
@@ -0,0 +1,6 @@
+//@ sourceURL=assets/scripts/js/quests/tutorial/respawnPackage.js
+//line above is for IntelliJ debugging purposes
+
+(function () {
+
+})();
