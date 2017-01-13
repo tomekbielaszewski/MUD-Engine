b3569cdbca0785e7140dcc8fa0e3188605c603f3
Grizwold <tomekbielaszewski@gmail.com>
2017-01-13 21:37:11 +0100

    first and second package can be collected by a player which has not brought them yet

---
+++ assets/scripts/js/quests/tutorial/canPickupPackageSecond.js
@@ -0,0 +1,40 @@
+//@ sourceURL=assets/scripts/js/quests/tutorial/canPickupPackageSecond.js
+//line above is for IntelliJ debugging purposes
+
+loadScript("tutorial-quest-ids");
+
+(function () {
+  function hasPackage() {
+    return game.player.hasItems(Quest.ITEM.package.id);
+  }
+
+  function alreadyBroughtThisPackage() {
+    return game.player.hasParameter(Quest.ID.secondPackageBrought);
+  }
+
+  function alreadyBroughtBothPackages() {
+    return game.player.hasParameter(Quest.ID.firstPackageBrought) &&
+      game.player.hasParameter(Quest.ID.secondPackageBrought);
+  }
+
+  function canPickup() {
+    if (hasPackage()) {
+      game.player.message("Masz już jeden pakunek w rękach!");
+      return false;
+    }
+
+    if (alreadyBroughtBothPackages()) {
+      game.player.message("Robota wykonana! Nie musisz już nosić tych pakunków.");
+      return false;
+    }
+
+    if (alreadyBroughtThisPackage()) {
+      game.player.message("Już to zaniosłeś! Poszukaj w innej części ładowni.");
+      return false;
+    }
+
+    return true;
+  }
+
+  return canPickup();
+})();
