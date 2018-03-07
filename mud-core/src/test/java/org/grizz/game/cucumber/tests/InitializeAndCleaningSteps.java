8d3d67d071f4a1840df576d529542cb50712abaa
tomekbielaszewski <tomekbielaszewski@gmail.com>
2018-03-07 23:14:34 +0100

    resetting notifier mock before every test

--- mud-core/src/test/java/org/grizz/game/cucumber/tests/InitializeAndCleaningSteps.java
+++ mud-core/src/test/java/org/grizz/game/cucumber/tests/InitializeAndCleaningSteps.java
@@ -3,10 +3,16 @@ package org.grizz.game.cucumber.tests;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.grizz.game.cucumber.CucumberTest;
+import org.grizz.game.service.notifier.Notifier;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.grizz.game.cucumber.GameConstants.STARTING_LOCATION;
 
 public class InitializeAndCleaningSteps extends CucumberTest {
+
+    @Autowired
+    private Notifier notifier;
 
     @Before
     public void init() {
@@ -14,6 +20,8 @@ public class InitializeAndCleaningSteps extends CucumberTest {
         mongo.save(createPlayer(PLAYER1, STARTING_LOCATION));
         mongo.save(createPlayer(PLAYER2, STARTING_LOCATION));
         mongo.save(createPlayer(PLAYER3, STARTING_LOCATION));
+
+        Mockito.reset(notifier);
     }
 
     @After
