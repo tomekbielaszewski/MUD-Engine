2a73a6c1648d7f05fd15e5ed598275298a7c189b
tomekbielaszewski <tomekbielaszewski@gmail.com>
2019-12-28 00:27:08 +0100

    fake sql queries attached to repositories (daos)

--- mud-core/src/main/java/org/grizz/game/model/repository/PlayerRepository.java
+++ mud-core/src/main/java/org/grizz/game/model/repository/PlayerRepository.java
@@ -1,19 +1,27 @@
 package org.grizz.game.model.repository;
 
 import org.grizz.game.model.Player;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
 
-public interface PlayerRepository /*extends CrudRepository<Player, String>*/ {
+public interface PlayerRepository {
+    @SqlQuery("SELECT * FROM 1")
     Player findByName(String name);
 
+    @SqlQuery("SELECT * FROM 1")
     Player findByNameIgnoreCase(String name);
 
+    @SqlQuery("SELECT * FROM 1")
     List<Player> findByLastActivityTimestampGreaterThan(long timestamp);
 
+    @SqlQuery("SELECT * FROM 1")
     List<Player> findByCurrentLocation(String id);
 
+    @SqlUpdate("SELECT * FROM 1")
     void update(Player player);
 
-    void save(Player player);
+    @SqlUpdate("SELECT * FROM 1")
+    void insert(Player player);
 }
