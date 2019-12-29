9eb1869e1b6bef03d56433ab42cdbe5833fc79f3
tomekbielaszewski <tomekbielaszewski@gmail.com>
2019-12-30 00:25:52 +0100

    dao sql queries

--- mud-core/src/main/java/org/grizz/game/model/db/dao/PlayerStatsDao.java
+++ mud-core/src/main/java/org/grizz/game/model/db/dao/PlayerStatsDao.java
@@ -1,9 +1,28 @@
 package org.grizz.game.model.db.dao;
 
 import org.grizz.game.model.db.entities.PlayerStatsEntity;
+import org.grizz.game.model.db.mappers.PlayerStatsEntityMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 
 public interface PlayerStatsDao {
-    PlayerStatsEntity getByName(String name);
+    @UseRowMapper(PlayerStatsEntityMapper.class)
+    @SqlQuery("SELECT *\n" +
+            "FROM player_stats\n" +
+            "WHERE player_name = :name;")
+    PlayerStatsEntity getByName(@Bind String name);
 
-    void upsert(PlayerStatsEntity playerStatsEntity);
+    @SqlUpdate("INSERT INTO player_stats\n" +
+            "VALUES (:p.playerName, :p.strength, :p.dexterity, :p.endurance, :p.intelligence, " +
+            " :p.wisdom, :p.charisma)\n" +
+            "ON CONFLICT (player_name) DO UPDATE SET strength     = excluded.strength,\n" +
+            "                                        dexterity    = excluded.dexterity,\n" +
+            "                                        endurance    = excluded.endurance,\n" +
+            "                                        intelligence = excluded.intelligence,\n" +
+            "                                        wisdom       = excluded.wisdom,\n" +
+            "                                        charisma     = excluded.charisma;")
+    void upsert(@BindBean("p") PlayerStatsEntity playerStatsEntity);
 }
--- mud-core/src/main/java/org/grizz/game/model/db/dao/PlayersDao.java
+++ mud-core/src/main/java/org/grizz/game/model/db/dao/PlayersDao.java
@@ -1,18 +1,43 @@
 package org.grizz.game.model.db.dao;
 
 import org.grizz.game.model.db.entities.PlayerEntity;
+import org.grizz.game.model.db.mappers.PlayerEntityMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PlayersDao {
-    PlayerEntity getByName(String name);
+    @UseRowMapper(PlayerEntityMapper.class)
+    @SqlQuery("SELECT * " +
+            "FROM players " +
+            "WHERE name = :name;")
+    PlayerEntity getByName(@Bind String name);
 
-    List<String> findByLastActivityTimestampGreaterThan(long timestamp);
+    @SqlQuery("SELECT name " +
+            "FROM players " +
+            "WHERE last_activity > TO_TIMESTAMP(:timestamp / 1000);")
+    List<String> findByLastActivityTimestampGreaterThan(@Bind long timestamp);
 
-    List<String> findByCurrentLocation(String id);
+    @SqlQuery("SELECT name " +
+            "FROM players " +
+            "WHERE current_location_id = :id;")
+    List<String> findByCurrentLocation(@Bind String id);
 
+    @SqlQuery("SELECT name " +
+            "FROM players " +
+            "WHERE name = :name;")
     Optional<String> checkExistence(String name);
 
-    void upsert(PlayerEntity playerEntity);
+    @SqlUpdate("INSERT INTO players " +
+            "VALUES (:p.name, :p.currentLocationId, :p.pastLocationId, TO_TIMESTAMP(:p.lastActivity / 1000)) " +
+            "ON CONFLICT (name) DO UPDATE SET name = excluded.name, " +
+            "                                 current_location_id = excluded.current_location_id, " +
+            "                                 past_location_id = excluded.past_location_id, " +
+            "                                 last_activity = excluded.last_activity;")
+    void upsert(@BindBean("p") PlayerEntity playerEntity);
 }
